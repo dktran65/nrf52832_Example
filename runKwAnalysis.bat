452f42e58f9df4115451761a450b22c1d1bd0fbb
Dzuy Tran <dktran65@gmail.com>
2020-01-09 14:12:05 -0800

    Update script files, and remove other unnecessary files

--- runKwAnalysis.bat
+++ runKwAnalysis.bat
@@ -1,6 +1,8 @@
+@echo off
+@set KW_TOOL_LOC=C:\Klocwork\kwbuildtools\bin\
 :: Run Cleanup and Klocwork Analysis
 ::
-C:\Klocwork\kwbuildtools\bin\kwinject cmd /c build.bat
+%KW_TOOL_LOC%\kwinject cmd /c build.bat
 ::
 :: Run KwBuildProject
 kwbuildproject --url http://hss-01:8080/SES_nrf52832 -f -o kwtables kwinject.out
