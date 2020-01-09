d8948a66cd26981faf07cf1811f889481600d1c1
Dzuy Tran <dktran65@gmail.com>
2020-01-09 14:49:12 -0800

    Update improved Batch Script

--- runKwAnalysis.bat
+++ runKwAnalysis.bat
@@ -1,12 +1,18 @@
 @echo off
+:: Setup KW Project and Server Location
+@set KW_PROJECT=SES_nrf52832
+@set KW_SVRNAME=hss-01
+@set KW_SVRPORT=8080
+@set KW_VERSION=Kw19v2
+
 @set KW_TOOL_LOC=C:\Klocwork\kwbuildtools\bin\
 :: Run Cleanup and Klocwork Analysis
 ::
 %KW_TOOL_LOC%\kwinject cmd /c build.bat
 ::
 :: Run KwBuildProject
-kwbuildproject --url http://hss-01:8080/SES_nrf52832 -f -o kwtables kwinject.out
+kwbuildproject --url http://%KW_SVRNAME%:%KW_SVRPORT%/%KW_PROJECT% -f -o kwtables kwinject.out
 ::
 ::
-kwadmin --url http://hss-01:8080 load SES_nrf52832 kwtables
+kwadmin --url http://%KW_SVRNAME%:%KW_SVRPORT% load %KW_PROJECT% kwtables --name %DATE:~10,4%%DATE:~4,2%%DATE:~7,2%_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%_%KW_VERSION%
 ::
