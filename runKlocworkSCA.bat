1ccab2c8a0c1a9b38accb070d7d89b64ba4cf7af
Dzuy Tran <dktran65@gmail.com>
2020-01-28 18:19:54 -0800

    Update new runKlocworkSCA batch file

---
+++ runKlocworkSCA.bat
@@ -0,0 +1,32 @@
+setlocal
+@echo on
+:: Setup KW Project and Server Location
+@set KW_PROJECT=SES_nrf52832
+@set KW_SVRNAME=hss-01
+@set KW_SVRPORT=8080
+@set KW_VERSION=Kw19v2
+@set KW_TOOL_LOC=C:\Klocwork\kwbuildtools\bin
+::
+:: Setup Seggar the Toolchain
+@Set UTIL_DIR=C:\SEGGER_Embedded_Studio_for_ARM_4.40\bin
+@Set UTIL_EMB=%UTIL_DIR%\emBuild
+:: Setup Project Location 
+@set PROJDIR=%CD%
+@Set OUTDIR="Output"
+::
+:: 
+:: Run Clean
+@cd %PROJDIR%
+if exist %OUTDIR% (@rmdir /Q/S %OUTDIR%)
+::
+:: Run kwinject - to trace the SES Native Build, and capturing the build specification
+%KW_TOOL_LOC%\kwinject --overwrite %UTIL_EMB% -config "Debug" nrf52832_Example.emProject
+::
+:: Run KwBuildProject
+kwbuildproject --url http://%KW_SVRNAME%:%KW_SVRPORT%/%KW_PROJECT% -f -o kwtables kwinject.out
+::
+::
+kwadmin --url http://%KW_SVRNAME%:%KW_SVRPORT% load %KW_PROJECT% kwtables --name %DATE:~10,4%%DATE:~4,2%%DATE:~7,2%_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%_%KW_VERSION%
+::
+endlocal
+@echo on
